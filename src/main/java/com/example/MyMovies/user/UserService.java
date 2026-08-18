e8e85231137d0941d31e027b493597b7c7de200c
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 17:44:32 +0200

    Deletes email field from User class.
    Signed-off-by: erikcerne <235554237+erikcerne@users.noreply.github.com>

--- src/main/java/com/example/MyMovies/user/UserService.java
+++ src/main/java/com/example/MyMovies/user/UserService.java
@@ -4,4 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    UserRepository jpa;
+
+    public UserService(UserRepository jpa) {
+        this.jpa = jpa;
+    }
+
+    public boolean isExistingUser(String id) {
+        return jpa.existsById(id);
+    }
 }
