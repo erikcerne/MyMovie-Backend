54bb298fa90b6909d67e9eeb9932b69f9ac1213a
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 12:28:22 +0200

    to uppercase

--- src/main/java/com/example/MyMovies/tmdb/tmdbDtos/TmdbReviewsDto.java
+++ src/main/java/com/example/MyMovies/tmdb/tmdbDtos/TmdbReviewsDto.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record TmdbReviewsDto(String content, String createdAt, authorDetails authorDetails) {
+public record TmdbReviewsDto(String content, String createdAt, AuthorDetails authorDetails) {
 }
