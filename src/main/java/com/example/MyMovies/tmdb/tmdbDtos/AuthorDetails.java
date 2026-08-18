5a1b46d005f183df87de01137e47a65fc68cb339
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 12:27:29 +0200

    to uppercase

---
+++ src/main/java/com/example/MyMovies/tmdb/tmdbDtos/AuthorDetails.java
@@ -0,0 +1,4 @@
+package com.example.MyMovies.tmdb.tmdbDtos;
+
+public record AuthorDetails(String name, String username, String rating) {
+}
--- src/main/java/com/example/MyMovies/tmdb/tmdbDtos/authorDetails.java
+++
@@ -1,4 +0,0 @@
-package com.example.MyMovies.tmdb.tmdbDtos;
-
-public record authorDetails(String name, String username, String rating) {
-}
