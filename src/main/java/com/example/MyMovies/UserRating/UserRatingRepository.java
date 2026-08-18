9591d0e20b41172f1aee465c2967bba7fca41f1a
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 21:23:19 +0200

    Adds query to `UserRatingRepository` to find all ratings by user ID.
    Signed-off-by: erikcerne <235554237+erikcerne@users.noreply.github.com>

--- src/main/java/com/example/MyMovies/UserRating/UserRatingRepository.java
+++ src/main/java/com/example/MyMovies/UserRating/UserRatingRepository.java
@@ -2,7 +2,9 @@ package com.example.MyMovies.UserRating;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRatingRepository extends JpaRepository<UserRating, UUID> {
+    List<UserRating> findAllByUser_UserId(String userUserId);
 }
