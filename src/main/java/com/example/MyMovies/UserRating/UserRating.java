42ebb6a8aaabd9c20b2dfd88a2a2aac5809ded1a
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 19:10:46 +0200

    Adds `date` field to UserRating for tracking review timestamp.
    Signed-off-by: erikcerne <235554237+erikcerne@users.noreply.github.com>

--- src/main/java/com/example/MyMovies/UserRating/UserRating.java
+++ src/main/java/com/example/MyMovies/UserRating/UserRating.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -32,4 +33,5 @@ import java.util.UUID;
 
     private int rating;
     private String comment;
+    private LocalDate date;
 }
