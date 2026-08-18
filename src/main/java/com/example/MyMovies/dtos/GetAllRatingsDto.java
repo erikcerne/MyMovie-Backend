a58253458fcf3c5396ec994b43b063e12c4a0521
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 21:23:31 +0200

    Removes unused `id` field from `GetAllRatingsDto`.
    Signed-off-by: erikcerne <235554237+erikcerne@users.noreply.github.com>

--- src/main/java/com/example/MyMovies/dtos/GetAllRatingsDto.java
+++ src/main/java/com/example/MyMovies/dtos/GetAllRatingsDto.java
@@ -2,6 +2,6 @@ package com.example.MyMovies.dtos;
 
 import java.time.LocalDate;
 
-public record GetAllRatingsDto(String content, int rating, long tmdbId, LocalDate date, Long id, String originalTitle,
+public record GetAllRatingsDto(String content, int rating, long tmdbId, LocalDate date, String originalTitle,
                                String posterPath, Double popularity) {
 }
