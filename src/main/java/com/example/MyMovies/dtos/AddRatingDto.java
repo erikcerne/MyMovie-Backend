44261e79cbe50b6ee5a0f2e70bcc54c356615f36
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 19:09:15 +0200

    Generalizes AddRatingDto `comment` to `content` and removes `userId`.
    Signed-off-by: erikcerne <235554237+erikcerne@users.noreply.github.com>

--- src/main/java/com/example/MyMovies/dtos/AddRatingDto.java
+++ src/main/java/com/example/MyMovies/dtos/AddRatingDto.java
@@ -1,6 +1,4 @@
 package com.example.MyMovies.dtos;
 
-import java.util.UUID;
-
-public record AddRatingDto(UUID userId, String comment, int rating, long tmdbId) {
+public record AddRatingDto(String content, int rating, long tmdbId) {
 }
