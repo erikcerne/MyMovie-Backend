a4d88c00908a5b0ca8bc04ae99a6779f8b9ec217
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-22 13:21:48 +0200

    Add MovieDetailsLogInDto for user movie review.

---
+++ src/main/java/com/example/MyMovies/dtos/MovieDetailsLogInDto.java
@@ -0,0 +1,7 @@
+package com.example.MyMovies.dtos;
+
+import com.example.MyMovies.UserMovie.WatchStatus;
+import com.example.MyMovies.tmdb.tmdbDtos.TmdbMovieDto;
+
+public record MovieDetailsLogInDto(TmdbMovieDto tmdbMovieDto, WatchStatus watchStatus, String content, int rating) {
+}
