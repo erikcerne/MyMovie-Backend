b03ee3431714cd64270ab246da24007e19feec3f
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-22 12:49:40 +0200

    Add method to find user movie by ID.

--- src/main/java/com/example/MyMovies/UserMovie/UserMovieRepository.java
+++ src/main/java/com/example/MyMovies/UserMovie/UserMovieRepository.java
@@ -11,4 +11,6 @@ public interface UserMovieRepository extends JpaRepository<UserMovie, UUID> {
     boolean existsUserMovieByUser_UserIdAndMovie_TmdbId(String userUserId, long movieTmdbId);
 
     void deleteByMovie_TmdbIdAndUser_UserId(long movieTmdbId, String userUserId);
+
+    UserMovie findByUser_UserIdAndMovie_TmdbId(String userUserId, long movieTmdbId);
 }
